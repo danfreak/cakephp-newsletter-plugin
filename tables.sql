3cc6bd5f906e0f9bf601c315a3ff504212153daa
danfreak <dan@freakclimbing.com>
2009-07-08 21:37:53 +0800

    correcting DB schema with missing columns
    Signed-off-by: Fabio Kreusch <fabiokr@gmail.com>

--- tables.sql
+++ tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE  `newsletter_subscriptions` (
   `modified` datetime default NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `newsletter_groups` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -16,7 +16,7 @@ CREATE TABLE  `newsletter_groups` (
   `created` datetime default NULL,
   `modified` datetime default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `newsletter_groups_subscriptions` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -24,7 +24,7 @@ CREATE TABLE  `newsletter_groups_subscriptions` (
   `newsletter_group_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `Foreign_Keys` (`newsletter_subscription_id`,`newsletter_group_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `newsletter_mails` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -38,7 +38,7 @@ CREATE TABLE  `newsletter_mails` (
   `created` datetime default NULL,
   `modified` datetime default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `newsletter_mail_views` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -47,7 +47,7 @@ CREATE TABLE  `newsletter_mail_views` (
   `created` datetime default NULL,
   PRIMARY KEY  (`id`),
   KEY `fk` (`newsletter_mail_id`,`ip`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `newsletter_groups_mails` (
   `id` int(10) unsigned NOT NULL auto_increment,
@@ -55,4 +55,4 @@ CREATE TABLE  `newsletter_groups_mails` (
   `newsletter_group_id` int(10) unsigned NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `fk` (`newsletter_mail_id`,`newsletter_group_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
